8504de9993a5be2478b516aa98f1bca81c3b2b50
Gad3lan <alan.adamiak@gmail.com>
2019-02-27 12:07:14 +0100

    Fin du TP4

--- TP4/complexes.cpp
+++ TP4/complexes.cpp
@@ -2,20 +2,77 @@
 
 using namespace std;
 
-void saisie(int &a, int &b) {
+const float EPSILON = 1e-6;
+
+struct Complexe {
+	float re, im;
+};
+
+void saisie(Complexe &c) {
 	cout << "Saisir la valeur de a: ";
-	cin >> a;
+	cin >> c.re;
 	cout << "Saisir la valeur de b: ";
-	cin >> b;
+	cin >>c.im;
 }
 
-void affiche(int a, int b) {
-	cout << a << " + i" << b << endl;
+void affiche(Complexe c) {
+	cout << c.re << " + " << c.im << "i" << endl;
 }
 
-void somme(int &a1, int &b1, int a2, int b2) {
-	a1 += a2;
-	b1 += b2;
+Complexe somme(Complexe c1, Complexe c2) {
+	return {c1.re + c2.re, c1.im + c2.im};
 }
 
-void produit(int &a1, int &b1, int a2, int b2) 
+Complexe sub(Complexe c1, Complexe c2) {
+	return {c1.re-c2.re, c1.im - c2.im};
+}
+
+void conjugue(Complexe &c) {
+	c.im *= -1;
+}
+
+Complexe produit(Complexe c1, Complexe c2)  {
+	return {(c1.re*c2.re) - (c1.im*c2.im), (c1.re*c2.im) + (c1.im*c2.re)};
+}
+
+Complexe div(Complexe c, float d) {
+	return {c.re/d, c.im/d};
+}
+
+float norme_carre(Complexe c) {
+	return c.re*c.re + c.im*c.im;
+}
+
+Complexe inverse(Complexe c) {
+	Complexe conj = c;
+	conjugue(conj);
+	return div(conj, norme_carre(c));
+}
+
+Complexe sqrt(Complexe c) {
+	Complexe u = {c.re, EPSILON};
+	while(norme_carre(sub(produit(u, u), c))/norme_carre(c) > EPSILON*EPSILON)
+		u = div(somme(u, produit(c,inverse(u))), 2);
+	return u;
+}
+
+int main() {
+	Complexe c = {1, 2};
+	Complexe c2 = {2, 1};
+	Complexe conj = c;
+	conjugue(c);
+	cout << "Affichage des 2 complexes :" << endl;
+	affiche(c);
+	affiche(c2);
+	cout << "Somme des 2 complexes :" << endl;
+	affiche(somme(c, c2));
+	cout << "Conjugué du premier complexe :" << endl;
+	affiche(conj);
+	cout << "Produit des 2 complexes :" << endl;
+	affiche(produit(c, c2));
+	cout << "Inverse du premier complexe :" << endl;
+	affiche(inverse(c));
+	cout << "Racine carre du premier complexe :" << endl;
+	affiche(sqrt(c));
+}
+
